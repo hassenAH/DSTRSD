243e684f67221dace5e13329a10bf4e02752e8da
AHMADI Hassen <61461149+hassenAH@users.noreply.github.com>
2025-09-21 00:23:00 +0100

    Update RequireAuth.tsx

--- src/routes/RequireAuth.tsx
+++ src/routes/RequireAuth.tsx
@@ -1,6 +1,7 @@
 // src/routes/RequireAuth.tsx
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
+import DashboardPage from "../pages/Dashboard/Dashboard";
 
 export default function RequireAuth() {
     const { isAuthenticated, loading } = useAuth();
@@ -11,5 +12,5 @@ export default function RequireAuth() {
     if (!isAuthenticated) {
         return <Navigate to="/" replace state={{ from: location }} />;
     }
-    return <Outlet />;
+    return <DashboardPage />;
 }
